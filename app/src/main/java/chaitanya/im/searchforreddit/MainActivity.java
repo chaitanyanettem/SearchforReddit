9b37a9e276db9cf25ffb27162ce63b4dc2db7cb4
Chaitanya Nettem <chaitanyanettem@gmail.com>
2016-06-07 23:37:04 +0530

    receives intent. buggy because new instances are created everytime.

--- app/src/main/java/chaitanya/im/searchforreddit/MainActivity.java
+++ app/src/main/java/chaitanya/im/searchforreddit/MainActivity.java
@@ -1,7 +1,10 @@
 package chaitanya.im.searchforreddit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity.java", "onCreate");
+
+        ((TextView) findViewById(R.id.temp_textview)).setText("Share text/links from other apps");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        intent.getFlags();
+        Log.d("onResume - intent: ",intent.toString());
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type!=null) {
+            Log.d("MainActivity.java", "Intent verified");
+            TextView displayText = (TextView) findViewById(R.id.temp_textview);
+            if ("text/plain".equals(type)) {
+                handleSendText(intent, displayText);
+            }
+        }
+    }
+
+    void handleSendText(Intent intent, TextView displayText) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        Log.d("MainActivity.java", sharedText);
+        if (sharedText != null) {
+            displayText.setText(sharedText);
+        }
     }
 }
+
