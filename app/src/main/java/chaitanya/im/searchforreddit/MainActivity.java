0bbf7a351ec1ceabe38f61263dcab9aa1ef32081
Chaitanya Nettem <chaitanyanettem@gmail.com>
2016-06-08 01:22:35 +0530

    Fixed multiple instances of app issue
    - set launchmode to singleInstance
    - implemented onNewIntent()
    - Updated gradle

--- app/src/main/java/chaitanya/im/searchforreddit/MainActivity.java
+++ app/src/main/java/chaitanya/im/searchforreddit/MainActivity.java
@@ -8,27 +8,42 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView displayText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity.java", "onCreate");
 
-        ((TextView) findViewById(R.id.temp_textview)).setText("Share text/links from other apps");
+        displayText = ((TextView) findViewById(R.id.temp_textview));
+        Log.d("MainActivity.java", "onCreate");
+        assert(displayText !=null);
+
+        displayText.setText("Share text/links from other apps");
+        Intent intent = getIntent();
+        receiveIntent(intent);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Intent intent = getIntent();
+        Log.d("MainActivity.java", "onResume");
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("MainActivity.java", "onNewIntent");
+        receiveIntent(intent);
+    }
+
+    void receiveIntent(Intent intent) {
         String action = intent.getAction();
         intent.getFlags();
-        Log.d("onResume - intent: ",intent.toString());
+        Log.d("MainActivity.java", "receiveIntent() toString - " + intent.toString());
         String type = intent.getType();
 
         if (Intent.ACTION_SEND.equals(action) && type!=null) {
-            Log.d("MainActivity.java", "Intent verified");
-            TextView displayText = (TextView) findViewById(R.id.temp_textview);
+            Log.d("MainActivity.java", "receiveIntent() - " + "Intent verified");
             if ("text/plain".equals(type)) {
                 handleSendText(intent, displayText);
             }
@@ -37,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     void handleSendText(Intent intent, TextView displayText) {
         String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-        Log.d("MainActivity.java", sharedText);
+        Log.d("MainActivity.java", "Shared Text:" + sharedText);
         if (sharedText != null) {
             displayText.setText(sharedText);
         }
