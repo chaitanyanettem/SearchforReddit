95483e57294477dd90c1f0fa2836ab3141a4f28d
Chaitanya Nettem <chaitanyanettem@gmail.com>
2016-06-30 14:26:14 +0530

    Replaced ActionBar with Toolbar
    - Added shadow for prelollipop
    - Switched to white colors
    - Switched to white statusbar and black icons for post M.

--- app/src/main/java/chaitanya/im/searchforreddit/LauncherActivity.java
+++ app/src/main/java/chaitanya/im/searchforreddit/LauncherActivity.java
@@ -2,6 +2,8 @@ package chaitanya.im.searchforreddit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
 
 public class LauncherActivity extends AppCompatActivity {
@@ -10,5 +12,7 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
