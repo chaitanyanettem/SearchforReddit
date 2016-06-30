6e505718ea4ac3d786fbb220e0b3933c8db03a14
Chaitanya Nettem <chaitanyanettem@gmail.com>
2016-06-30 14:43:08 +0530

    differentiated shadows for api<21 and api>=21

--- app/src/main/java/chaitanya/im/searchforreddit/LauncherActivity.java
+++ app/src/main/java/chaitanya/im/searchforreddit/LauncherActivity.java
@@ -1,9 +1,13 @@
 package chaitanya.im.searchforreddit;
 
+import android.content.res.Resources;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
 
 public class LauncherActivity extends AppCompatActivity {
@@ -14,5 +18,17 @@ public class LauncherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_launcher);
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
+
+        // dp -> px : http://stackoverflow.com/a/9563438/1055475
+        Resources resources = getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float px = 4 * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            toolbar.setElevation(px);
+        }
+        else {
+            findViewById(R.id.shadow).setVisibility(View.VISIBLE);
+        }
     }
 }
