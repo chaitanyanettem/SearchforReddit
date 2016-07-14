9b3c876bb5b6203ed3c93d81e930d28b68c4cecc
Chaitanya Nettem <chaitanyanettem@gmail.com>
2016-07-14 20:20:57 +0530

    Lots of changes, including removal of email address prompt from launcher.
    - Addition of Fragment
    - tweaks with colors and themes
    - addition of icons

--- app/src/main/java/chaitanya/im/searchforreddit/LauncherActivity.java
+++ app/src/main/java/chaitanya/im/searchforreddit/LauncherActivity.java
@@ -1,34 +1,60 @@
 package chaitanya.im.searchforreddit;
 
+import android.accounts.AccountManager;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
 import android.util.DisplayMetrics;
-import android.util.TypedValue;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LauncherActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_EMAIL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        EditText searchEditText = (EditText) findViewById(R.id.search_edit_text);
         setSupportActionBar(toolbar);
+
+        // To allow for multiline EditText with imeOptions set to actionSearch
+        searchEditText.setHorizontallyScrolling(false);
+        searchEditText.setMaxLines(Integer.MAX_VALUE);
 
         // dp -> px : http://stackoverflow.com/a/9563438/1055475
         Resources resources = getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
-        float px = 4 * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        float px = 2 * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             toolbar.setElevation(px);
         }
         else {
             findViewById(R.id.shadow).setVisibility(View.VISIBLE);
+        }
+/*
+        try {
+            Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                    new String[] { GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE }, false, null, null, null, null);
+            startActivityForResult(intent, REQUEST_CODE_EMAIL);
+        } catch (ActivityNotFoundException e) {
+            // TODO
+        }
+*/
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_EMAIL && resultCode == RESULT_OK) {
+            String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
         }
     }
 }
