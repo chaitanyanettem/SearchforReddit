0a41a9be136b4375047032907e6083656670cd1e
Chaitanya Nettem <chaitanyanettem@gmail.com>
2016-10-15 21:45:43 +0530

    What's new dialog

--- app/src/main/java/chaitanya/im/searchforreddit/StringResources.java
+++ app/src/main/java/chaitanya/im/searchforreddit/StringResources.java
@@ -6,5 +6,9 @@ class StringResources {
             "<b>v1.65</b><br/>" +
             "Fixed the mobile youtube links issue. I'm still exploring how to solve this for other mobile urls. So stay tuned for more updates.<br/><br/>" +
             "<b>v1.55</b><br/>" +
-            "Fixes a bug on API 21 & 22 devices where the app crashed on start when launched from the launcher. This was because of missing attributes in the style.xml file for API 21 & 22.";
+            "Fixes a bug on API 21 & 22 devices where the app crashed on start when launched from the launcher. This was because of missing attributes in the style.xml file for API 21 & 22.<br/><br/>" +
+            "<b>v1.3</b><br/>" +
+            "New bug fixes. Fixes a bug which occurs when json response from reddit has bad fields. Next update will bring you dark theme.<br/><br/>" +
+            "<b>v1.2.2</b><br/>" +
+            "This version brings you an urgent bug fix fixing a bug which caused crashes in pre-Lollipop devices because of my use of vectorDrawables. (Sorry about that!)";
 }
